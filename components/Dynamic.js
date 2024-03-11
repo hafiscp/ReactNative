9541e4e57988ed44d7e2a536b6dfca6182c00d57
hafiscp <cphafis3@gmail.com>
2024-03-11 15:01:39 +0530

    Dimensions API - State

--- components/Dynamic.js
+++ components/Dynamic.js
@@ -1,24 +1,44 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function Dynamic({ children, style }) {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
   return (
-    <View style={[styles.bg, style]}>
-      <Text style={styles.text}>Hello</Text>
+    <View
+      style={[
+        styles.bg,
+        style,
+        {
+          height: windowHeight > 400 ? "50%" : 200,
+          width: windowWidth > 900 ? "60%" : 350,
+        },
+      ]}
+    >
+      <Text style={{ fontSize: windowWidth > 400 ? 30 : 24 }}>Hello</Text>
     </View>
   );
 }
 
-const boxWidth = Dimensions.get("window").width
-const boxHeight = Dimensions.get("window").height
+// const boxWidth = Dimensions.get("window").width;
+// const boxHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   bg: {
-    height: boxHeight > 400 ? "40%" : "200",
-    width: boxWidth > 200 ? "75%": "200",
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  text:{
-    fontSize:20
-  }
 });
