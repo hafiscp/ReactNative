ef1dbec24e7fb056312e75b178f7dd0686efa8ab
hafiscp <cphafis3@gmail.com>
2024-03-22 09:17:33 +0530

    onRefresh - DataFetch

--- App.js
+++ App.js
@@ -6,24 +6,43 @@ import {
   StatusBar,
   StyleSheet,
   FlatList,
+  ActivityIndicator,
 } from "react-native";
 
 import { useState, useEffect } from "react";
 
 export default function App() {
   const [postList, setPostList] = useState([]);
+  const [loading, setIsLoading] = useState(true);
+  const [refreshing, setIsRefreshing] = useState(false);
 
-  const fetchData = async (limit = 10) => {
+  const fetchData = async (limit = 5) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     );
     let data = await response.json();
     setPostList(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.loading}>
+        <ActivityIndicator size="large" color="blue" />
+        <Text>Loading</Text>
+      </SafeAreaView>
+    );
+  }
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    fetchData(15);
+    setIsRefreshing(false);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -33,6 +52,7 @@ export default function App() {
           renderItem={({ item }) => {
             return (
               <View style={styles.itemListContainer}>
+                <Text style={styles.bodyText}>{item.id}</Text>
                 <Text style={styles.titleText}>{item.title}</Text>
                 <Text style={styles.bodyText}>{item.body}</Text>
               </View>
@@ -48,6 +68,8 @@ export default function App() {
           ListFooterComponent={
             <Text style={styles.listHeader}>End of List</Text>
           }
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
         />
       </View>
     </SafeAreaView>
@@ -81,5 +103,11 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  loading: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
